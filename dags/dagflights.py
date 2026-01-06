84cd3fa60ce65442c741aa1283a3829153e326d5
Mateus Almeida <mateusalmeida881@gmail.com>
2026-01-06 19:16:17 -0300

    feature: adicao do primeiro step da dag para ingestao de dados

--- dags/dagflights.py
+++ dags/dagflights.py
@@ -1,6 +1,7 @@
 from airflow.sdk import dag
 from pendulum import datetime
 from datetime import timedelta
+import json
 from airflow.providers.amazon.aws.operators.lambda_function import LambdaInvokeFunctionOperator
 
 default_args = {
@@ -13,11 +14,25 @@ default_args = {
 
 @dag(
     start_date=datetime(2025, 10, 31),
-    end_date=datetime(2025, 11, 30),
+    end_date=datetime(2025, 11, 2),
+    catchup=True,
     schedule="@daily",
     tags=["flights-anac"],
     max_active_runs=2,
     default_args=default_args
 )
 def pipeline_flights_anac():
-    pass
+
+    extract_data = LambdaInvokeFunctionOperator(
+        task_id= "invoke_lambda_extract",
+        function_name= "lambda_extract_api",
+        invocation_type= "RequestResponse",
+        aws_conn_id= "AWS",
+        payload=json.dumps({"layer": "staging",
+                            "bucket": "mateus-us-east-1-etl-flights",
+                            "dt_voo": "{{ logical_date.strftime('%d%m%Y') }}"
+                            })
+    )
+
+
+pipeline_flights_anac()
